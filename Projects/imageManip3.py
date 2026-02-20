3246219063fb709c798e28aecf471dfb4777e500
AlloyFoot <111826686+AlloyFoot@users.noreply.github.com>
2026-02-20 16:39:52 -0500

    Add files via upload

---
+++ Projects/imageManip3.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("image5.jpg")
+
+
+
+small = cv2.resize(image, (200, 200))
+medium = cv2.resize(image, (400, 400))
+large = cv2.resize(image, (600, 600))
+
+cv2.imshow("Original Image", image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("Small Image (200x200)", small)
+cv2.imwrite("input_image_small.jpg", small)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("Medium Image (400x400)", medium)
+cv2.imwrite("input_image_medium.jpg", medium)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("Large Image (600x600)", large)
+cv2.imwrite("input_image_large.jpg", large)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
